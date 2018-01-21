df5f9df9e11cc44d45d535b82d16770499beb402
Xioqua <1350212416@qq.com>
2018-01-21 09:53:23 +0800

    再次验证调试方法

--- js/controllers.js
+++ js/controllers.js
@@ -9,5 +9,5 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
       {'name': 'MOTOROLA XOOM™',
        'snippet': 'The Next, Next Generation tablet.'}
     ];
-    $scope.orderProp = 'age';
+    // $scope.orderProp = 'age';
   });
