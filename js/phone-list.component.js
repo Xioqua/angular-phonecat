1624d03edab9d061a87ee3b44ff1ab5d7de3ee35
Xioqua <1350212416@qq.com>
2018-01-21 14:56:18 +0800

    看了官github我决定也像它拆分组件

--- js/phone-list.component.js
+++ js/phone-list.component.js
@@ -1,5 +1,3 @@
-var phonecatApp = angular.module('phonecatApp', []);
-
 phonecatApp.controller('PhoneListCtrl', function ($scope,$http) {
    $http.get('../json/phone.json').then(function(result) { 
      $scope.phones = result.data;
