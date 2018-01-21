c2cdb3d8cff5981f59e2dc4441e364d8e60da46c
Xioqua <1350212416@qq.com>
2018-01-21 10:54:36 +0800

    在ng上册数json格式错误,并且这个方法能够取得想要的json数据

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,8 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope,$http) {
    $http.get('../json/phone.json').then(function(result) { 
-     $scope.phones = result.data;
+    //  $scope.phones = result.data;
+     $scope.phones = result.data.splice(0, 5);
     });
     $scope.orderProp = 'age';
   });
